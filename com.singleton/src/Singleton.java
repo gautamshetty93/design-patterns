6f0d99552f51f74112368cf5539615d7d06fa40a
gautamshetty93 <gautam.shetty3@gmail.com>
2026-02-20 22:33:31 +0530

    Adding singleton design pattern

---
+++ com.singleton/src/Singleton.java
@@ -0,0 +1,14 @@
+package com.singleton.src;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
